4f7da7ae9cab680e2fd4e6621fda3f4971b3df1c
SRG <emailsouza@gmail.com>
2023-04-11 15:21:31 -0600

    2023-04-11 added search on the backend and frontend

--- app/Components/Courses.jsx
+++ app/Components/Courses.jsx
@@ -1,22 +1,26 @@
 import Link from 'next/link';
 
-async function fetchCourses() {
-  const response = await fetch('http://localhost:3000/api/courses');
-  const data = await response.json();
-  return data;
-}
+// Used this at first, when demonstrating how to use fetch() from the server side of Next.js
+// async function fetchCourses() {
+//   const response = await fetch('http://localhost:3000/api/courses');
+//   const data = await response.json();
+//   return data;
+// }
 
-const Courses = async () => {
-  const courses = await fetchCourses();
+const Courses = ({ courses }) => {
+  // const courses = await fetchCourses(); //used with fetch() from server side of Next.js
 
   return (
     <>
-    <div className="courses">Courses</div>
+    <div className="courses"></div>
       { courses.map(course => (
         <div key={course.id} className="card">
           <h2>{course.title}</h2>
-          <small>Level: { </small>
+          <small>Level: { course.level } </small>
           <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go to course
+          </Link>
         </div>
       ))}
     </>
--- app/page.js
+++ app/page.js
@@ -1,15 +1,35 @@
+'use client';
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading";
 import Link from "next/link";
+import Courses from "./Components/Courses";
+import CourseSearch from "./Components/CourseSearch";
 
 const HomePage = () => {
-  return <div>
-    <h1> HOoray! </h1>
-    <ul>
-      <li><Link href='/'>Home </Link></li>
-      <li><Link href='/about'>About </Link></li>
-      <li><Link href='/about/team'>Team </Link></li>
-    </ul>
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+    fetchCourses();
+  }, []);
 
-  </div>; 
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <>
+      <h1>Welcome</h1>
+      <CourseSearch getSearchResults={(results) => setCourses(results)}/>
+      <Courses courses={courses} />
+    </>
+  );
 };
 
 export default HomePage;
