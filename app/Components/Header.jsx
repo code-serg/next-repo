36923fcb99ac3a823cd5352e507cf0b1e4d57f7d
srg <emailsouza@gmail.com>
2023-10-02 18:55:08 -0600

    prelim styling

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,20 +1,25 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/logo-rs.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <div className="logo">
-          <Link href="/"> CodeSRG</Link>
+          <Link href="/">
+            <Image src={logo} alt="repo-search-logo" width={60} height={60} />
+            CodeSRG
+          </Link>
         </div>
-        <div className="links"> 
+        <div className="links">
           <Link href="/about"> About</Link>
           <Link href="/about/team"> The Team</Link>
           <Link href="/code/repos"> Code</Link>
         </div>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
